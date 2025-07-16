65574b061a5b76184b09cf493a5594b8db7f79c1
Yurii <j.zherebilov15@gmail.com>
2025-07-17 02:00:45 +0300

    їбав в жопу тайпскріпт

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,36 +17,108 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+const SORT_FIELD_NAME = 'name';
+const SORT_FIELD_LENGTH = 'length';
+
 export const App: React.FC = () => {
+  const [visibleGoods, setVisibleGoods] = useState([...goodsFromServer]);
+  const [reversed, setReversed] = useState(false);
+  const [sortField, setSortField] = useState('');
+
+  let visibleGoodsCopy = [...visibleGoods];
+
+  // const sortByName = () => {
+  //   setVisibleGoods(visibleGoodsCopy.toSorted((a, b) => a.localeCompare(b)));
+  //   setSortField(SORT_FIELD_NAME);
+  // };
+
+  // const sortByLength = () => {
+  //   setVisibleGoods(visibleGoodsCopy.toSorted((a, b) => a.length - b.length));
+  //   setSortField(SORT_FIELD_LENGTH);
+  // };
+
+  const reset = () => {
+    setVisibleGoods(goodsFromServer);
+    setSortField('');
+    setReversed(false);
+  };
+
+  const showResetButton = sortField || reversed;
+
+  // eslint-disable-next-line default-case
+  switch (sortField) {
+    case SORT_FIELD_NAME:
+      visibleGoodsCopy.sort((a, b) => a.localeCompare(b));
+      break;
+
+    case SORT_FIELD_LENGTH:
+      visibleGoodsCopy.sort((a, b) => a.length - b.length);
+      break;
+  }
+
+  if (reversed) {
+    visibleGoodsCopy = visibleGoodsCopy.toReversed();
+  }
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={cn('button is-info', {
+            'is-light': sortField !== SORT_FIELD_NAME,
+          })}
+          // onClick={sortByName}
+          onClick={() => setSortField(SORT_FIELD_NAME)}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={cn('button is-info', {
+            'is-light': sortField !== SORT_FIELD_LENGTH,
+          })}
+          // onClick={sortByLength}
+          onClick={() => setSortField(SORT_FIELD_LENGTH)}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          onClick={() => setReversed(!reversed)}
+          className={cn('button is-info', {
+            'is-light': !reversed,
+          })}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
+        {showResetButton && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        )}
+        {/* <button
+          type="button"
+          className="button is-danger is-light"
+          onClick={reset}
+        >
           Reset
-        </button>
+        </button> */}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {visibleGoodsCopy.map(good => (
+          <li className="good" data-cy="Good" key={good}>
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
